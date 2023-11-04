23fbd6525db1b0f03aa5a9770d0660bc5450fb59
Mazin Fouad <m.fouad@live.de>
2023-11-04 12:23:45 +0100

    user can add favorite item to the cart

--- src/app/components/favorite-items/favorite-items.component.ts
+++ src/app/components/favorite-items/favorite-items.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/apiData/cart.service';
 import { SharedService } from 'src/app/services/apiData/shared.service';
+import { AuthService } from 'src/app/services/user/auth.service';
 
 @Component({
   selector: 'app-favorite-items',
@@ -10,9 +12,15 @@ import { SharedService } from 'src/app/services/apiData/shared.service';
 })
 export class FavoriteItemsComponent implements OnInit, OnDestroy {
   favoriteItems: Product[] = [];
+  itemsInCart: any[] = [];
   private favoritesSubscription: Subscription = new Subscription();
+  selectedSizeValue: string = '';
 
-  constructor(private sharedService: SharedService) {}
+  constructor(
+    private sharedService: SharedService,
+    private authService: AuthService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     this.sharedService.getFavorites$.subscribe((data: any) => {
@@ -20,6 +28,32 @@ export class FavoriteItemsComponent implements OnInit, OnDestroy {
     });
   }
 
+  addToCart(favoriteItem: Product, selectedSize: string): void {
+    const userId = this.authService.getUserId();
+    const date = new Date().toISOString().split('T')[0];
+
+    const existingProductIndex = this.itemsInCart.findIndex(
+      (item) => item.product === favoriteItem.id && item.size === selectedSize
+    );
+
+    if (existingProductIndex !== -1) {
+      this.itemsInCart[existingProductIndex].quantity += 1;
+    } else {
+      this.itemsInCart.push({
+        product: favoriteItem.id,
+        quantity: 1,
+        size: selectedSize,
+      });
+    }
+
+    this.cartService
+      .addToCart(userId!, date, this.itemsInCart)
+      .subscribe((response) => {
+        console.log(response);
+        // Update the shared service with the new cart items
+        this.sharedService.updateItemsInCart(this.itemsInCart);
+      });
+  }
   ngOnDestroy(): void {
     if (this.favoritesSubscription) {
       this.favoritesSubscription.unsubscribe();
